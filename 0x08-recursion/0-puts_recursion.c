dc2566b1f87114f06d028b1aba678f4c82921f94
Mavrutto <maverick1999only@gmail.com>
2023-08-30 13:17:28 -0700

    Task0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _puts_recursion - entry point
+ * @s:variable
+ * Return:void
+ */
+void _puts_recursion(char *s)
+{
+	if( *s != '\0')
+	{
+		putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		putchar('\n');
+	}
+}
