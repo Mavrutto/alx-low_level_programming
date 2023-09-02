f58812cbe9ae4d09455a3ee8a00b1787f1359c27
Mavrutto <maverick1999only@gmail.com>
2023-09-01 18:13:24 -0700

    Task3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main- entry 
+ * @argc:number
+ * @argv: pointer
+ * Return:0
+ */
+int main(int argc, char **argv)
+{
+int i, j;
+if (argc != 3)
+{
+	printf("Error\n");
+	return (1);
+}
+else
+i = atoi(argv[1]);
+j = atoi(argv[2]);
+printf("%d\n", i * j);
+return (0);
+}
